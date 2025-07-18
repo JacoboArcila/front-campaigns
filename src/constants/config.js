dbd1f7e9819d8cc3ea1dd2ec2ef861f766eec1a2
JacoboArcila <wagner.arcila.jacobo@gmail.com>
2025-07-18 11:07:48 -0500

    UPDATE

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,16 +1,16 @@
 // Configuración de APIs por plataforma
 export const API_CONFIG = {
   lefty: {
-    //url: 'http://api3.prismgrp.com/', // API diferente para lefty
-    url: 'http://apist2.prismgrp.com/', // API diferente para lefty
-    useDefaultApi: false
+    url: 'http://api3.prismgrp.com/', // API diferente para lefty
+    //url: 'http://apist2.prismgrp.com/', // API diferente para lefty
+    useDefaultApi: false,
   },
   traackr: {
-    useDefaultApi: true
+    useDefaultApi: true,
   },
   talkwalker: {
-    useDefaultApi: true
-  }
+    useDefaultApi: true,
+  },
 };
 
 export const DEFAULT_API_URL = 'http://apist2.prismgrp.com';
